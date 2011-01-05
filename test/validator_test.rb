8101329e549bd9aecf29f1acbaac89026eac81cc
Andy Gregorowicz <andy@mitre.org>
2011-01-05 13:42:52 -0500

    slightly better testing

--- test/validator_test.rb
+++ test/validator_test.rb
@@ -8,6 +8,9 @@ class ValidatorTest < Test::Unit::TestCase
     c32 = File.read('fixtures/demographics.xml')
     results = validator.validate(c32)
     assert results
+    # Uncomment this line to print the error messages from schematron
+    # puts results.map{|r| r[:error_message]}.join("\n")
+    assert_equal 5, results.size
   end
   
   def test_xml_schema
@@ -15,5 +18,6 @@ class ValidatorTest < Test::Unit::TestCase
     c32 = File.read('fixtures/demographics.xml')
     results = validator.validate(c32)
     assert results
+    assert_equal 1, results.size
   end
 end
