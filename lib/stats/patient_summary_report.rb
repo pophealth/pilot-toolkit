b0910900712f253998816f68f2018191f3ddc242
Andy Gregorowicz <andy@mitre.org>
2011-06-01 11:52:22 -0400

    Simple implementation of creating a PatientSummaryReport from a C32

--- lib/stats/patient_summary_report.rb
+++ lib/stats/patient_summary_report.rb
@@ -2,6 +2,19 @@ module Stats
   class PatientSummaryReport
     attr_accessor :allergies, :care_goals, :conditions, :encounters, :immunizations, :medical_equipment,
                   :medications, :procedures, :results, :social_history, :vital_signs
+
+    def self.from_c32(document)
+      psr = PatientSummaryReport.new
+      pi = QME::Importer::PatientImporter.instance
+      patient_entry_hash = pi.create_c32_hash(document)
+      patient_entry_hash.each_pair do |section, entry_list|
+        pss = PatientSummarySection.new
+        entry_list.each {|entry| pss.add_entry(entry)}
+        psr.send("#{section}=", pss)
+      end
+
+      psr
     end
+
   end
 end
