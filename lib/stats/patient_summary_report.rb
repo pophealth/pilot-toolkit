2eb9544e938e3e2444ff6a412bc511fdf492da75
Andy Gregorowicz <andy@mitre.org>
2011-06-01 16:36:47 -0400

    Switching the code system names to match the QME

--- lib/stats/patient_summary_report.rb
+++ lib/stats/patient_summary_report.rb
@@ -6,17 +6,17 @@ module Stats
                   :medications, :procedures, :results, :social_history, :vital_signs
 
     @mu_code_sets = {}
-    @mu_code_sets[:allergies] = ["RxNorm","SNOMEDCT"]
-    @mu_code_sets[:care_goals] = ["SNOMEDCT"]
-    @mu_code_sets[:conditions] = ["SNOMEDCT", "ICD9", "ICD10"]
+    @mu_code_sets[:allergies] = ["RxNorm","SNOMED-CT"]
+    @mu_code_sets[:care_goals] = ["SNOMED-CT"]
+    @mu_code_sets[:conditions] = ["SNOMED-CT", "ICD-9-CM", "ICD-10-CM"]
     @mu_code_sets[:encounters] = ["CPT"]
     @mu_code_sets[:immunizations] = ["RxNorm","CVX"]
-    @mu_code_sets[:medical_equipment] = ["SNOMEDCT"]
+    @mu_code_sets[:medical_equipment] = ["SNOMED-CT"]
     @mu_code_sets[:medications] = ["RxNorm","CVX"]
-    @mu_code_sets[:procedures] = ["CPT","ICD9","ICD10","HCPCS"]
-    @mu_code_sets[:results] = ["LOINC","SNOMEDCT"]
-    @mu_code_sets[:social_history] = ["SNOMEDCT"]
-    @mu_code_sets[:vital_signs] = ["ICD9","ICD10","SNOMEDCT"]
+    @mu_code_sets[:procedures] = ["CPT","ICD-9-CM","ICD-10-CM","HCPCS"]
+    @mu_code_sets[:results] = ["LOINC","SNOMED-CT"]
+    @mu_code_sets[:social_history] = ["SNOMED-CT"]
+    @mu_code_sets[:vital_signs] = ["ICD-9-CM","ICD-10-CM","SNOMED-CT"]
 
     def self.from_c32(document)
       psr = PatientSummaryReport.new
