61dc6a18d84230ff1083d51fa75b15394d0a872c
Rob McCready <rob@mitre.org>
2011-06-06 20:36:54 -0400

    Code cleanup

--- lib/stats/patient_summary_report.rb
+++ lib/stats/patient_summary_report.rb
@@ -1,20 +1,15 @@
-
-#require 'quality-measure-engine'
-#require 'patient_summary_section'
-#require 'ccrscan'
-
-
 module Stats
-  # PatientSummaryReport is used to capture an assesment of coded values within both the HITSP C32 and ASTM CCR documents that are to be input 
-  # to popHealth
+
+  # PatientSummaryReport is used to capture an assesment of coded values within both the HITSP C32 
+  # and ASTM CCR documents that are to be input to popHealth
   class PatientSummaryReport
+
     attr_accessor :allergies, :care_goals, :conditions, :encounters, :immunizations, :medical_equipment,
                   :medications, :procedures, :results, :social_history, :vital_signs
 
-
     # Each section is initialized with the appropriate meaningful use (MU) coding systems
     def initialize
-        # Initialize the sections of the Patient Summary
+      # Initialize the sections of the Patient Summary
       @@mu_code_sets = {}
       @@mu_code_sets[:allergies] = ["RxNorm","SNOMEDCT"]
       @@mu_code_sets[:care_goals] = ["SNOMEDCT"]
@@ -27,8 +22,7 @@ module Stats
       @@mu_code_sets[:results] = ["LOINC","SNOMEDCT"]
       @@mu_code_sets[:social_history] = ["SNOMEDCT"]
       @@mu_code_sets[:vital_signs] = ["ICD-9-CM","ICD-10-CM","SNOMEDCT"]
-   end
-
+    end
 
     def self.from_c32(document)
       psr = PatientSummaryReport.new
@@ -36,15 +30,15 @@ module Stats
       patient_entry_hash = pi.create_c32_hash(document)
       patient_entry_hash.each_pair do |section, entry_list|
         pss = PatientSummarySection.new(section, @@mu_code_sets[section])
-        entry_list.each do |entry| 
-                pss.add_entry(entry) 
-#                STDERR.puts "desc = #{entry.description}"
+        entry_list.each do |entry|
+          pss.add_entry(entry)
         end
         psr.send("#{section}=", pss)
       end
-     psr
+      psr
     end
-   def self.from_ccr(document)
+
+    def self.from_ccr(document)
       psr = PatientSummaryReport.new
       pi = CCRscan::CCR.new
       patient_entry_hash = pi.create_ccr_hash(document)
@@ -57,123 +51,118 @@ module Stats
       psr
     end
 
-   ### THERE HAS GOT TO BE A BETTER WAY TO DO THIS!
-   def dump
-         STDERR.puts "***BEGIN DUMP***"
-         if(@allergies) 
-            @allergies.dump(STDERR)
-         end
-         if(@care_goals)
-                @care_goals.dump(STDERR)
-         end
-         if(@conditions)
-                @conditions.dump(STDERR)
-         end
-         if (@encounters)
-                @encounters.dump(STDERR)
-         end
-         if (@immunizatons)
-         @immunizatons.dump(STDERR)
-         end
-         if (@medical_equipment)
-         @medical_equipment.dump(STDERR)
-         end
-         if (@medications)
-         @medications.dump(STDERR)
-         end
-         if (@procedures)
-         @procedures.dump(STDERR)
-          end
-        if (@results)
-         @results.dump(STDERR)
-         end
-         if (@social_history)
-         @social_history.dump(STDERR)
-         end
-         if (@vital_signs)
-         @vital_signs.dump(STDERR)
-         end
-         STDERR.puts "***END DUMP***"
-   end
+    def dump
+      if(@allergies)
+         @allergies.dump(STDERR)
+      end
+      if(@care_goals)
+        @care_goals.dump(STDERR)
+      end
+      if(@conditions)
+        @conditions.dump(STDERR)
+      end
+      if (@encounters)
+        @encounters.dump(STDERR)
+      end
+      if (@immunizatons)
+        @immunizatons.dump(STDERR)
+      end
+      if (@medical_equipment)
+        @medical_equipment.dump(STDERR)
+      end
+      if (@medications)
+        @medications.dump(STDERR)
+      end
+      if (@procedures)
+        @procedures.dump(STDERR)
+      end
+      if (@results)
+        @results.dump(STDERR)
+      end
+      if (@social_history)
+        @social_history.dump(STDERR)
+      end
+      if (@vital_signs)
+        @vital_signs.dump(STDERR)
+      end
+    end
 
-   def summary
-        summary_hash = {}
-         if(@allergies) 
-            summary_hash.merge!(@allergies.summary)
-         end
-         if(@care_goals)
-                summary_hash.merge!(@care_goals.summary)
-         end
-         if(@conditions)
-                summary_hash.merge!(@conditions.summary)
-         end
-         if (@encounters)
-                summary_hash.merge!(@encounters.summary)
-         end
-         if (@immunizatons)
-                summary_hash.merge!(@immunizatons.summary)
-         end
-         if (@medical_equipment)
-                summary_hash.merge!(@medical_equipment.summary)
-         end
-         if (@medications)
-                summary_hash.merge!(@medications.summary)
-         end
-         if (@procedures)
-                summary_hash.merge!(@procedures.summary)
-          end
-        if (@results)
-                summary_hash.merge!(@results.summary)
-         end
-         if (@social_history)
-                summary_hash.merge!(@social_history.summary)
-         end
-         if (@vital_signs)
-                summary_hash.merge!(@vital_signs.summary)
-         end
-      return summary_hash
+    def summary
+      summary_hash = {}
+      if(@allergies) 
+        summary_hash.merge!(@allergies.summary)
+      end
+      if(@care_goals)
+        summary_hash.merge!(@care_goals.summary)
+      end
+      if(@conditions)
+        summary_hash.merge!(@conditions.summary)
+      end
+      if (@encounters)
+        summary_hash.merge!(@encounters.summary)
+      end
+      if (@immunizatons)
+        summary_hash.merge!(@immunizatons.summary)
+      end
+      if (@medical_equipment)
+        summary_hash.merge!(@medical_equipment.summary)
+      end
+      if (@medications)
+        summary_hash.merge!(@medications.summary)
+      end
+      if (@procedures)
+        summary_hash.merge!(@procedures.summary)
+      end
+      if (@results)
+        summary_hash.merge!(@results.summary)
+      end
+      if (@social_history)
+        summary_hash.merge!(@social_history.summary)
+      end
+      if (@vital_signs)
+        summary_hash.merge!(@vital_signs.summary)
+      end
+      summary_hash
+    end
 
-   end
-   def unique_non_mu_entries
-        summary_hash = {}
-         if(@allergies) 
-            summary_hash.merge!(@allergies.unique_non_mu_entries)
-         end
-         if(@care_goals)
-                summary_hash.merge!(@care_goals.unique_non_mu_entries)
-         end
-         if(@conditions)
-                summary_hash.merge!(@conditions.unique_non_mu_entries)
-         end
-         if (@encounters)
-                summary_hash.merge!(@encounters.unique_non_mu_entries)
-         end
-         if (@immunizatons)
-                summary_hash.merge!(@immunizatons.unique_non_mu_entries)
-         end
-         if (@medical_equipment)
-                summary_hash.merge!(@medical_equipment.unique_non_mu_entries)
-         end
-         if (@medications)
-                summary_hash.merge!(@medications.unique_non_mu_entries)
-         end
-         if (@procedures)
-                summary_hash.merge!(@procedures.unique_non_mu_entries)
-          end
-        if (@results)
-                summary_hash.merge!(@results.unique_non_mu_entries)
-         end
-         if (@social_history)
-                summary_hash.merge!(@social_history.unique_non_mu_entries)
-         end
-         if (@vital_signs)
-                summary_hash.merge!(@vital_signs.unique_non_mu_entries)
-         end
-      return summary_hash
+    def unique_non_mu_entries
+      summary_hash = {}
+      if(@allergies) 
+        summary_hash.merge!(@allergies.unique_non_mu_entries)
+      end
+      if(@care_goals)
+        summary_hash.merge!(@care_goals.unique_non_mu_entries)
+      end
+      if(@conditions)
+        summary_hash.merge!(@conditions.unique_non_mu_entries)
+      end
+      if (@encounters)
+        summary_hash.merge!(@encounters.unique_non_mu_entries)
+      end
+      if (@immunizatons)
+        summary_hash.merge!(@immunizatons.unique_non_mu_entries)
+      end
+      if (@medical_equipment)
+        summary_hash.merge!(@medical_equipment.unique_non_mu_entries)
+      end
+      if (@medications)
+        summary_hash.merge!(@medications.unique_non_mu_entries)
+      end
+      if (@procedures)
+        summary_hash.merge!(@procedures.unique_non_mu_entries)
+      end
+      if (@results)
+        summary_hash.merge!(@results.unique_non_mu_entries)
+      end
+      if (@social_history)
+        summary_hash.merge!(@social_history.unique_non_mu_entries)
+      end
+      if (@vital_signs)
+        summary_hash.merge!(@vital_signs.unique_non_mu_entries)
+      end
+      summary_hash
+    end
 
-   end
-
-
-end
+  end
 
 end
--- lib/stats/patient_summary_section.rb
+++ lib/stats/patient_summary_section.rb
@@ -1,47 +1,49 @@
-
 require 'quality-measure-engine'
 
 # Stats object collects statistics regarding the content of a single section of a Patient Summary (C32 or CCR)
-# As entries are added to the section, they are classified as coded vs uncoded, and within the coded as MU 
-# (meaningful use) coded, or alien (not the relevant code set for meaningful use clinical quality measures). 
+# As entries are added to the section, they are classified as coded vs uncoded, and within the coded as MU
+# (meaningful use) coded, or alien (not the relevant code set for meaningful use clinical quality measures).
 module Stats
 
-# NEED TO ADD A + operator that merges the Codes hash?
-# get rid of subclass and just make a class with count, description and codes...much easier
+  # NEED TO ADD A + operator that merges the Codes hash?
+  # get rid of subclass and just make a class with count,
+  # description and codes...much easier
   class StatsEntry < QME::Importer::Entry
-      attr_accessor :count, :codes
+
+    attr_accessor :count, :codes
 
     def initialize
-         count = 1
+      count = 1
     end
 
     def count
-       @count
+      @count
     end
+
     def self.fromEntry(entry)
-       sentry = Stats::StatsEntry.new
-       sentry.codes = entry.codes
-       sentry.description = entry.description
-       sentry.count = 1
-       return sentry
+      sentry = Stats::StatsEntry.new
+      sentry.codes = entry.codes
+      sentry.description = entry.description
+      sentry.count = 1
+      return sentry
     end
 
     def add(entry)
-        raise "Only entries with the same description can be added" unless entry.description == @description
-        @count += entry.count
-        entry.codes.each_pair do | codeset, values|
-           codes[codeset] ||= []  # if it doesn't exist add it
-           codes[codeset] = values.to_set.union(codes[codeset].to_set).to_a   # result is the union of codes for this codeset
-        end
+      raise "Only entries with the same description can be added" unless entry.description == @description
+      @count += entry.count
+      entry.codes.each_pair do |codeset, values|
+        codes[codeset] ||= []  # if it doesn't exist add it
+        codes[codeset] = values.to_set.union(codes[codeset].to_set).to_a   # result is the union of codes for this codeset
+      end
     end
-    def dump(outfp)
-       outfp.puts "StatsEntry:   description = #{@description}   count = #{@count}"
-        codes.each_pair do | codeset, values|
-           outfp.puts "\tcodeset #{codeset}  values #{values.join(',')}"
-        end
-    end
-   end
 
+    def dump(outfp)
+      outfp.puts "StatsEntry:   description = #{@description}   count = #{@count}"
+      codes.each_pair do |codeset, values|
+        outfp.puts "\tcodeset #{codeset}  values #{values.join(',')}"
+      end
+    end
+  end
 
   class PatientSummarySection
 
@@ -58,84 +60,72 @@ module Stats
       @alien_coded_entries = []
       @mu_code_systems = mu_code_systems
     end
-    
-# Return a hash entry that includes a hash of entries with unique desciprions within this section
-# If there are no entries, the hash is empty.
+
+    # Return a hash entry that includes a hash of entries with unique desciprions within this section
+    # If there are no entries, the hash is empty.
     def unique_non_mu_entries
-        # if there are no entries, return an empty hash
-        if(uncoded_entries.size + alien_coded_entries.size == 0)
-                return {}
+
+      # if there are no entries, return an empty hash
+      if(uncoded_entries.size + alien_coded_entries.size == 0)
+        return {}
+      end
+
+      unique_entries = {@name => { "mucodesystems" => @mu_code_systems, "entries" => {} } }
+      uhash = unique_entries[@name]["entries"]
+
+      uncoded_entries.each do |entry|
+        sentry = Stats::StatsEntry.fromEntry(entry)
+        if(uhash[sentry.description])
+          uhash[sentry.description].add(sentry)
+        else
+          uhash[sentry.description] = sentry
         end
-        STDERR.puts "uncoded = #{uncoded_entries.size} alien = #{alien_coded_entries.size}"
-        unique_entries = { @name => { "mucodesystems" => @mu_code_systems,
-                                     "entries" => {}
-                                   }
-                          }
-        uhash = unique_entries[@name]["entries"]
+      end
 
-        uncoded_entries.each do | entry |
-                sentry = Stats::StatsEntry.fromEntry(entry)
-               if(uhash[sentry.description])
-                uhash[sentry.description].add(sentry)
-               else
-                uhash[sentry.description] = sentry
-               end
+      alien_coded_entries.each do |entry|
+        sentry = Stats::StatsEntry.fromEntry(entry)
+        if(uhash[sentry.description])
+          uhash[sentry.description].add(sentry)
+        else
+          uhash[sentry.description] = sentry
         end
-        alien_coded_entries.each do | entry |
-               sentry = Stats::StatsEntry.fromEntry(entry)
-              if(uhash[sentry.description])
-                uhash[sentry.description].add(sentry)
-               else
-                uhash[sentry.description] = sentry
-               end
-         end
+      end
 
-#        uhash.each_pair do | desc, entry |
-#            STDERR.puts "desc= #{desc}   count = #{entry.count} codes = #{entry.codes}"
-#        end
+      uhash.each_pair do |desc, entry|
+        if(entry.codes.size > 0)
+          uhash[desc] = {"count" => entry.count, "codes" => entry.codes }
+        else
+          uhash[desc] = {"count" => entry.count}
+        end
+      end
 
-       uhash.each_pair do | desc, entry |
-         if(entry.codes.size > 0)
-                uhash[desc] = { "count" => entry.count,
-                                "codes" => entry.codes }
-         else
-                 uhash[desc] = { "count" => entry.count }
-         end
-          
-       end
-
-       return unique_entries
+      unique_entries
     end
 
-#
     def summary
-       results = { @name => { "entries" => num_coded_entries + num_uncoded_entries,
-                             "mu code systems" => @mu_code_systems,
-                             "coded entries" => num_coded_entries,
-                             "mu coded entries" => num_mu_coded_entries,
-                             "mu code systems in use" => mu_code_systems_found,
-                             "non-mu coded entries" =>num_alien_coded_entries,
-                             "non-mu code systems in use" => alien_code_systems_found
-                            }
-                  }
-        return results
-     end
+      results = { @name => { "entries" =>                     num_coded_entries + num_uncoded_entries,
+                             "mu code systems" =>             @mu_code_systems,
+                             "coded entries" =>               num_coded_entries,
+                             "mu coded entries" =>            num_mu_coded_entries,
+                             "mu code systems in use" =>      mu_code_systems_found,
+                             "non-mu coded entries" =>        num_alien_coded_entries,
+                             "non-mu code systems in use" =>  alien_code_systems_found} }
+    end
 
     def dump(outfp)
-   outfp.puts "Section #{@name}:   mu_code_systems = #{@mu_code_systems.join(',')}"
-   outfp.puts "\tEntries: #{num_coded_entries + num_uncoded_entries}, #{num_coded_entries} coded"
-    if(num_alien_coded_entries > 0)
+      outfp.puts "Section #{@name}:   mu_code_systems = #{@mu_code_systems.join(',')}"
+      outfp.puts "\tEntries: #{num_coded_entries + num_uncoded_entries}, #{num_coded_entries} coded"
+      if (num_alien_coded_entries > 0)
         outfp.puts "\taliens: #{num_alien_coded_entries} #{alien_code_systems_found}"
-    end
-    if(num_mu_coded_entries > 0)
+      end
+      if (num_mu_coded_entries > 0)
         andaliens = ""
-        if(alien_code_systems_found.size > 0)
-             andaliens = "and #{alien_code_systems_found}"
+        if (alien_code_systems_found.size > 0)
+          andaliens = "and #{alien_code_systems_found}"
         end
         outfp.puts "\tmu:     #{num_mu_coded_entries}   #{mu_code_systems_found} #{andaliens}"  
+      end
     end
-   end
-#
 
     def num_uncoded_entries
       uncoded_entries.size
@@ -198,6 +188,7 @@ if __FILE__ == $0
    entry.add_code(32001,"ICD10")
    entry.add_code(1,"GORK")
    section.add_entry(entry)
+
    entry1 = QME::Importer::Entry.new
    entry1.description = "test_entry 2"
    entry1.add_code(32000, "ICD9")
@@ -205,6 +196,7 @@ if __FILE__ == $0
    entry1.add_code(32000, "FOO1")
    entry1.add_code(32001,"BAR1")
    section.add_entry(entry)
+
    entry2 = QME::Importer::Entry.new
    entry2.description = "test_entry 3"
    entry2.add_code(32000, "FOO")
@@ -212,20 +204,23 @@ if __FILE__ == $0
    entry2.add_code(32000, "BAR")
    entry2.add_code(32001,"BAR1")
    section.add_entry(entry2)
-  entry3 = QME::Importer::Entry.new
+
+   entry3 = QME::Importer::Entry.new
    entry3.description = "test_entry 4"
    entry3.add_code(32002, "FOO")
    entry3.add_code(32004,"FOO")
    entry3.add_code(32006, "BAR")
    entry3.add_code(32008,"BAR1")
    section.add_entry(entry3)
+
    unique_non_mu_entries = section.unique_non_mu_entries
    unique_non_mu_entries.each_pair do | key, value |
-        STDERR.puts "key = #{key}   value = #{value}   valueclass = #{value.class}"
-        value.each_pair do | k,v|
-               STDERR.puts "\tkey = #{k}   value = #{v}   valueclass = #{v.class}"
-        end
+     STDERR.puts "key = #{key}   value = #{value}   valueclass = #{value.class}"
+     value.each_pair do | k,v|
+       STDERR.puts "\tkey = #{k}   value = #{v}   valueclass = #{v.class}"
+     end
    end
+
    section.dump(STDERR)
    STDERR.puts section.summary
    STDERR.puts JSON.pretty_generate(section.unique_non_mu_entries)
@@ -237,4 +232,3 @@ if __FILE__ == $0
    entrya.dump(STDERR)
 
 end
-
