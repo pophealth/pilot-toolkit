893903c33d0c78ea1983268a5e7cace2368550e9
Saul Kravitz <skravitz@mitre.org>
2011-06-14 16:35:00 -0400

    c32_preprocesor

--- lib/preprocess/c32_preprocessor.rb
+++ lib/preprocess/c32_preprocessor.rb
@@ -1,15 +1,11 @@
 
- #vitals_to_snomed
+ #C32Preprocessor
  # Patches certain LOINC codes found in C32 vitals section by adding a translation block to SNOMED-CT
  # SNOMED-CT codes are used by MU CQM, so this allows the coded data to be used by popHealth
    require 'nokogiri'
     require 'json'
 
 
-#Need to do this like the importer...have a list of sections, with entry XPaths, code XPaths, and description XPaths
-#@section_importers[:results] = SectionImporter.new("//cda:observation[cda:templateId/@root='2.16.840.1.113883.3.88.11.83.15.1'] | //cda:observation[cda:templateId/@root='2.16.840.1.113883.3.88.11.83.15']")
-#@section_importers[:vital_signs] = SectionImporter.new("//cda:observation[cda:templateId/@root='2.16.840.1.113883.3.88.11.83.14']")
-#def initialize(entry_xpath, code_xpath="./cda:code", status_xpath=nil, description_xpath="./cda:code/cda:originalText/cda:reference[@value] | ./cda:text/cda:reference[@value] ")
 
 module C32Preprocessor
   class PatchCodesDocument
@@ -34,7 +30,7 @@ module C32Preprocessor
       ids.each do |id|
         tag = id['ID']
         value = id.content
-#        STDERR.puts "tag = #{tag} value = #{value}"
+#       STDERR.puts "tag = #{tag} value = #{value}"
         @id_map[tag] = value
       end
     end
@@ -79,12 +75,12 @@ module C32Preprocessor
      code = code_element['code']
      codesystem = code_element['codeSystemName']
      d = code_element['codeSystem']
-#     STDERR.puts "add_code_translations section = #{@section} codesystem #{codesystem} code #{code}"
+     STDERR.puts "add_code_translations section = #{@section} codesystem #{codesystem} code #{code}"
      if(!@map || !@map[codesystem])
         return
       end
      l = @map[codesystem][code]
-#     STDERR.puts "l = #{l}"
+     STDERR.puts "l = #{l}"
      if(l) # If there is a translation, add a translate block
        l.each do | translation |
          add_translate_block(code_element,translation[1], translation[2], translation[0])
@@ -98,6 +94,7 @@ module C32Preprocessor
        end
        tag = description[0]['value']
        value = lookup_tag(tag)
+       STDERR.puts "FREE-TEXT value #{value}"
        # OK, now we have the description.   Let's look it up.
        l = @map["FREE-TEXT"][value]
        code_element = entry.at_xpath(@code_xpath)
@@ -105,7 +102,7 @@ module C32Preprocessor
          # If there is a code block, add a translate block
          # If it doesn't exist, punt
          l.each do | translation |
-#           STDERR.puts "translation = #{translation.join(',')}"
+           STDERR.puts "translation = #{translation.join(',')}"
              add_translate_block(code_element,translation[1], translation[2], translation[0])
          end  
        end
@@ -120,7 +117,7 @@ module C32Preprocessor
  #      STDERR.puts "Found #{entries.size} elements"
        entries.each do | entry|
          description = entry.xpath(@description_xpath)
-#         STDERR.puts "@description_xpath = #{@description_xpath} description = #{description} codes_xpath = #{@code_xpath}"
+         STDERR.puts "@description_xpath = #{@description_xpath} description = #{description} codes_xpath = #{@code_xpath}"
          codes = entry.xpath(@code_xpath)
 #         STDERR.puts "codes size = #{codes.size}"
          codes.each do |code|
